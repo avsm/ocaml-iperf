f40d6106050dc11b8c85ff0bbe521cdb57247c2d
Anil Madhavapeddy <anil@recoil.org>
2023-03-31 05:56:32 -0600

    proto: license and ocamldoc

--- src/proto.mli
+++ src/proto.mli
@@ -1,4 +1,4 @@
-
+(** Iperfv3 protocol handling functions. *)
 
 val cookie_size: int
 (** Length of the initial cookie sent on a new connection. *)
@@ -21,3 +21,18 @@ val params_length : Yojson.Safe.t -> int
 val params_parallel : Yojson.Safe.t -> int
 (** [params_parallel j] gets the amount of parallel connections from the params. *)
 
+(*{{{ Copyright (c) 2023 Anil Madhavapeddy <anil@recoil.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+  }}}*)
